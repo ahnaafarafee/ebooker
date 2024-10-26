f90d7b1984e2ae49a151dcdc24a0c7a8d0633c84
Ahnaaf Rafee <ahnaafarafee@gmail.com>
2024-10-26 18:45:04 +0600

    await in auth() clerk

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (!userId) {
     return NextResponse.json("Unauthorized", { status: 401 });
