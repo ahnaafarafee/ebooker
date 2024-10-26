5afbef5c8c9962ec31dc5862992b24ca52c1243a
Ahnaaf Rafee <ahnaafarafee@gmail.com>
2024-10-26 18:08:54 +0600

    add superbase

--- prisma/migrations/20241025161241_add_on_delete/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `book` DROP FOREIGN KEY `Book_authorId_fkey`;
-
--- AlterTable
-ALTER TABLE `book` MODIFY `text` TEXT NOT NULL;
-
--- AddForeignKey
-ALTER TABLE `Book` ADD CONSTRAINT `Book_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
